e200c62538f217154a13297af94edaedbd0a4628
kamalogudah <mcpaul2058@gmail.com>
2019-11-28 13:37:41 +0300

    Handling Errors Gracefully

--- src/index.js
+++ src/index.js
@@ -7,20 +7,29 @@ class App extends React.Component {
     super(props);
     // this is the only time we do direct assignment to
     // this.state
-    this.state = { lat: null};
+    this.state = { lat: null, errorMessage: '' };
 
     window.navigator.geolocation.getCurrentPosition(
       (position) => {
-        this.setState({lat: position.coords.latitude});
+        this.setState({ lat: position.coords.latitude });
       },
-      (err) => console.log(err)
+      (err) => {
+        this.setState({ errorMessage: err.message });
+      }
     );
 
   }
 
   render() {
-   
-    return <div> Latitude: { this.state.lat} </div>
+
+    return(
+      <div>
+        Latitude: { this.state.lat }
+        <br/>
+        Error: { this.state.errorMessage }
+       </div>
+
+    );
   }
 }
 
