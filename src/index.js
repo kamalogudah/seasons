bce6151a8a3746ae5f234966ea40cd1a91f0a8c4
kamalogudah <mcpaul2058@gmail.com>
2019-11-28 12:29:03 +0300

    Get a USER LOCATION

--- src/index.js
+++ src/index.js
@@ -2,6 +2,9 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 
 const App = () => {
+  window.navigator.geolocation.getCurrentPosition(
+    (position) => console.log(position)
+  );
   return (
     <div> Hi there</div>
 
