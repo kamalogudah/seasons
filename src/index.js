a916de9c5dd92d062c56040baf6fea479db03861
kamalogudah <mcpaul2058@gmail.com>
2019-11-29 13:32:44 +0300

    Showing Icons

--- src/index.js
+++ src/index.js
@@ -4,33 +4,34 @@ import SeasonDisplay from './SeasonDisplay';
 
 class App extends React.Component {
 
-  // constructor(props) {
-  //   super(props);
-  //   // this is the only time we do direct assignment to
-  //   // this.state
-  //   this.state = { lat: null, errorMessage: '' };
-  // }
-  state = { lat: null, errorMessage: '' };
+  constructor(props) {
+    super(props);
+    // this is the only time we do direct assignment to
+    // this.state
+    this.state = { lat: null, errorMessage: '' };
+  }
+  // state = { lat: null, errorMessage: '' };
   componentDidMount() {
     window.navigator.geolocation.getCurrentPosition(
       position => this.setState({ lat: position.coords.latitude }),
       err => this.setState({ errorMessage: err.message })
     );
 
-  }
+  };
 
   render() {
 
     if (this.state.errorMessage && !this.state.lat) {
-      return <div> Error: { this.state.errorMessage } </div>
+      return <div> Error: {this.state.errorMessage} </div>;
 
     }
 
     if (!this.state.errorMessage && this.state.lat) {
-      return <SeasonDisplay lat={this.state.lat}/>
+      return <SeasonDisplay lat={this.state.lat} />;
 
     }
-    return <div> Loading ..... </div>
+
+    return <div> Loading Please wait ... </div>;
   }
 }
 
